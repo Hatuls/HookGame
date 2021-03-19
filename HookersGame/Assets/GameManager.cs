4b20a2b8fdcce904622b48a4c6f71d79c71bc73d
Rei <reisegev@yahoo.com>
2021-03-19 13:39:52 +0200

    monosingleton

---
+++ HookersGame/Assets/GameManager.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class GameManager : MonoSingleton<GameManager>
+{
+    public override void Init()
+    {
+        Debug.Log("Hello");
+    }
+    private void Start()
+    {
+        Debug.Log(GameManager.Instance);
+    }
+}
---
+++ HookersGame/Assets/MonoSingleton.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public abstract class MonoSingleton<T> : MonoBehaviour ,IInit where T: MonoSingleton<T> 
+{
+    private static T m_Instance;
+    public static T Instance
+    {
+        get {
+            if (m_Instance == null)
+            {
+                m_Instance = GameObject.FindObjectOfType(typeof(T)) as T;
+                if (m_Instance == null)
+                    Debug.Log("Couldnt Find The Object of type " + typeof(T).ToString());
+            }
+              return m_Instance;
+        }
+    
+    }
+
+    private void Awake()
+    {
+        if (m_Instance == null)
+            m_Instance = this as T;
+        else if (m_Instance != this)
+        {
+            Debug.LogError("Another instance of " + GetType() + " is already exist! Destroying self...");
+            DestroyImmediate(this);
+            return;
+        }
+    }
+    public virtual void Init() { }
+}
+public interface IInit {
+
+    public void Init();
+}

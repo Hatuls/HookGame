519396d5e51ead920fa01688ae3155504a30b6ef
ronchabarr <ronchabarr@gmail.com>
2021-03-23 16:33:32 +0200

    physicsManager

---
+++ HookersGame/Assets/PlayerPhysicsManager.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+
+[Serializable]
+public class PlayerPhysicsManager 
+{
+
+    public enum PhysicsStates { fall, Swing, hang, leap, DefaultAir, DefaultFloor, wall,LastDontUseMe }
+    internal PhysicsStates physicsState;
+
+    [Serializable]
+    public class DragValues
+    {
+       public float DefaultFloor, DefaultAir, Swing, leap, hang, fall, wall;
+    }
+    [SerializeField] DragValues dragValues;
+
+    [Serializable]
+    public class StateValues 
+    {
+        [Header("State")]
+        public PhysicsStates stateRequest;
+        [Header("next State if there is else same")]
+        public PhysicsStates nextState;
+        public float TimeBetweenStates;
+
+    }
+    [SerializeField] StateValues[] StatesSettings=new StateValues[(int)PhysicsStates.LastDontUseMe-1];
+
+
+
+
+    PlayerManager _playerManager;
+    Rigidbody rb;
+
+
+
+
+    public void InitPhysics(Rigidbody referencedRb,PlayerManager playerManager)
+    {
+        _playerManager = playerManager;
+        rb = referencedRb;
+    }
+
+
+    public void CaulculatePhysics(bool grounded,bool grappled,bool wall)
+    {
+        if (grounded)
+        {
+            PhysicsRequest(PhysicsStates.DefaultFloor);
+        }
+        
+
+
+
+
+
+
+
+
+    }
+
+     void ChangeState(PhysicsStates newActiveState)
+    {
+        physicsState = newActiveState;
+        switch (newActiveState) 
+        {
+            
+            case PhysicsStates.DefaultAir:
+                rb.drag = dragValues.DefaultAir;
+                break;
+
+            case PhysicsStates.DefaultFloor:
+                rb.drag = dragValues.DefaultFloor;
+                break;
+
+
+            case PhysicsStates.fall:
+                rb.drag = dragValues.fall;
+                break;
+
+            case PhysicsStates.hang:
+                rb.drag = dragValues.hang;
+                break;
+
+            case PhysicsStates.leap:
+                rb.drag = dragValues.leap;
+                break;
+
+            case PhysicsStates.Swing:
+                rb.drag = dragValues.Swing;
+                break;
+
+
+            case PhysicsStates.wall:
+                rb.drag = dragValues.wall;
+                break;
+        }
+
+
+
+       
+    }
+     void PhysicsRequest(PhysicsStates stateRequested)
+    {
+        foreach(StateValues found in StatesSettings)
+        {
+            if (found.stateRequest == stateRequested)
+            {
+                _playerManager.StartCoroutine(PhysicsTransistor(stateRequested, found.nextState, found.TimeBetweenStates));
+            }
+        }
+       
+    }
+
+    IEnumerator PhysicsTransistor(PhysicsStates stateRequested, PhysicsStates nextState, float TimeBeforChange)
+    {
+
+        ChangeState(stateRequested);
+        yield return new WaitForSeconds(TimeBeforChange);
+        ChangeState(nextState);
+    }
+
+    
+
+
+    // Start is called before the first frame update
+   
+}
--- HookersGame/Assets/Scripts/Player/PlayerManager.cs
+++ HookersGame/Assets/Scripts/Player/PlayerManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public enum CharState { Mounted, UnMounted }
 public class PlayerManager : MonoSingleton<PlayerManager>
 {
+
+    [SerializeField] PlayerPhysicsManager playerPhysicsManager;
     CameraController _cameraController;
     PlayerController _playerController;
 
@@ -46,6 +48,7 @@ public class PlayerManager : MonoSingleton<PlayerManager>
         GetComponents();
         GetTechGun();
         GetCompressor();
+        playerPhysicsManager.InitPhysics(_playerController.rb, this);
        
     }
   
@@ -55,8 +58,14 @@ public class PlayerManager : MonoSingleton<PlayerManager>
         UpdateUi();
         CameraCommands();
         ApplyInputs();
-        ChangeDrag();
+        playerPhysicsManager.CaulculatePhysics(Grounded(),_heldTechGun.grappled,wallCheck());
+        
+        
     }
+
+   
+
+
     public void UpdateUi()
     {
         SpeedUi();
@@ -188,21 +197,21 @@ public class PlayerManager : MonoSingleton<PlayerManager>
     }
 
     #region PlayerCommands
-
-    public void ChangeDrag()
-    {
-        if (Grounded())
-        {
-            _playerController.rb.drag = DragVector.x;
-        }
-        else { _playerController.rb.drag = DragVector.y; }
-        if (_heldTechGun.grappled)
-        {
-            _playerController.rb.drag = DragVector.z;
-        }
+    
+    //public void ChangeDrag()
+    //{
+    //    if (Grounded())
+    //    {
+    //        _playerController.rb.drag = DragVector.x;
+    //    }
+    //    else { _playerController.rb.drag = DragVector.y; }
+    //    if (_heldTechGun.grappled)
+    //    {
+    //        _playerController.rb.drag = DragVector.z;
+    //    }
        
        
-    }
+    //}
    
     
     public void RecieveCharge(int ammount)
@@ -267,8 +276,12 @@ public class PlayerManager : MonoSingleton<PlayerManager>
 
     public bool Grounded()
     {
-        return Physics.Raycast(transform.position, Vector3.down, 2, GroundLayer);
+        return Physics.Raycast(transform.position, Vector3.down, 1, GroundLayer);
     }  
+    public bool wallCheck()
+    {
+        return true;
+    }
 }
 [System.Serializable]
 public class PlayerEffectMenu 

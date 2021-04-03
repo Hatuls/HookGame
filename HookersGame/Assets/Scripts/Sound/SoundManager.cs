9ad9e30d4f79b7b08a0a9f7cd3d0655bdce356b7
Rei <reisegev@yahoo.com>
2021-04-03 16:28:45 +0300

    added spawner

--- HookersGame/Assets/Scripts/Sound/SoundManager.cs
+++ HookersGame/Assets/Scripts/Sound/SoundManager.cs
@@ -22,12 +22,15 @@ public partial class SoundManager : MonoSingleton<SoundManager>
     public static event SoundEvent StopMusic;
    public  static float GetBeat => currentSong.GetBPM;
     public bool GetGameStartingCondition => gameStarted;
+    
+
     public override void Init()
     {
         _audioSource = GetComponent<AudioSource>();
         noteDestination = FindObjectOfType<NoteDestination>();
         currentSong = songs[0];
         AdjustAudioProfile(audioProfile);
+        VolumeBoxesSpawner.Instance.SpawnBuilding();
     }
     // Start is called before the first frame update
     public static float GetBeatPerSecond()
---
+++ HookersGame/Assets/Scripts/Sound/VolumeBoxesSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class VolumeBoxesSpawner : MonoBehaviour{
+
+    public static VolumeBoxesSpawner Instance;
+    int currentLevel;
+    [SerializeField] int AmountOfBoxes;
+    [SerializeField] float distanceBetweenBoxes;
+    [SerializeField] GameObject prefab;
+   [SerializeField] Vector3 leftStartPos, rightStartPos;
+    [SerializeField] Transform buildingHolder;
+    private void Awake()
+    {
+        Instance = this;
+    }
+    public void Init() { SpawnBuilding(); }
+
+    void SpawnBuilding() {
+        distanceBetweenBoxes = prefab.transform.localScale.x;
+        for (int i = 0; i < AmountOfBoxes; i++)
+        {
+            var leftBuilding = Instantiate(prefab, leftStartPos + Vector3.forward * i * distanceBetweenBoxes, Quaternion.identity, buildingHolder);
+            var rightBuilding = Instantiate(prefab, leftStartPos + Vector3.forward * i * distanceBetweenBoxes, Quaternion.identity, buildingHolder);
+        }
+    
+    }
+}

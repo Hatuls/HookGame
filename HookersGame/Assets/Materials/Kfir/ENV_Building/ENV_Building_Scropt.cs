2a76ef7778074f661a764afd973b06d631bc7372
KfirBal <80958617+KfirBal@users.noreply.github.com>
2021-05-15 23:24:35 +0300

    AWESOME

---
+++ HookersGame/Assets/Materials/Kfir/ENV_Building/ENV_Building_Scropt.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ENV_Building_Scropt : MonoBehaviour
+{
+    Material ENV_Building;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ENV_Building = GetComponent<MeshRenderer>().material;
+        setmaterialfloat("ForeGround_Lines_Speed", 0.1f, 1f);
+        setmaterialfloat("ForeGround_lines_Scale_X", 1f, 2f);
+        setmaterialfloat("ForeGround_Dots_Speed", 0.1f, 1f);
+        setmaterialfloat("ForeGround_Dots_Scale_X", 1f, 2f);
+        setmaterialfloat("BackGround_lines_Speed", 0.1f, 1f);
+        setmaterialfloat("BackGround_Lines_Scale_X", 1f, 2f);
+        setmaterialfloat("Background_Dots_Speed", 0.1f, 1f);
+        setmaterialfloat("BackGround_Dots_Scale_X", 1f, 2f);
+        
+    }
+    public void setmaterialfloat(string name, float min, float max) 
+    {
+        ENV_Building.SetFloat(name, Random.Range(min, max));
+    }
+   
+}

f81681a7a48ff22f29a3e18d8ca715029d401bca
ronchabarr <ronchabarr@gmail.com>
2021-08-10 13:35:00 +0300

    tutorial

---
+++ HookersGame/Assets/tutorialToggle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialToggle : MonoBehaviour
+{
+
+    [SerializeField] GameObject tutorialGo;
+    bool toggle = true;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (toggle) { tutorialGo.SetActive(false); toggle = false; }
+            else { tutorialGo.SetActive(true); toggle = true; }
+
+        }   
+    }
+}
